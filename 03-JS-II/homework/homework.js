33527c23fc6411b1f24147ce0749f00c6eea47e6
unknown <nicolima200@gmail.com>
2022-06-21 01:48:54 -0300

    Agregue el array de la tabladel seis

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -183,10 +183,12 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
+  //Escribe tu código aquí
+  let tabla=[]   
   for (let i=0; i<=10; i++){
-    return i*6;
+    tabla.push(i*6);
   }
+  return tabla;
 }
 
 function tieneTresDigitos(numero){
