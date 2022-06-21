46664eb298081addcdd7f2a3c3fe1a86b2d0438f
unknown <nicolima200@gmail.com>
2022-06-21 12:20:36 -0300

    MEses del año con .includes

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -128,7 +128,13 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let contador=0;
+  for (i=0;i<arreglo.length;i++){
+    if (arreglo[i]>18){
+      contador++
+    }
+  }
+  return contador;
 }
 
 
@@ -137,7 +143,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia===1 || numeroDeDia===7){
+    return "Es fin de semana";
+  }else{
+    return "Es dia laboral";
+  }
 } 
 
 
@@ -145,7 +155,8 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  str=n.toString;
+  return str[0]==9;
 }
 
 
@@ -153,6 +164,10 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  let n=arreglo[0];
+  for (i=1 ;i<arreglo.length; i++){
+    return n===arreglo[i];
+  }
   
 } 
 
@@ -162,6 +177,13 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let nuevoArray=[];
+  if (array.includes("Enero") && array.includes("Marzo") && array.includes("Noviembre")){
+      nuevoArray=array;
+      return nuevoArray;
+  }else{
+      return "No se encontraron los meses pedidos";
+  }
 }
 
 
