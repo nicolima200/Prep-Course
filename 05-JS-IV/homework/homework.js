b2c882aeb3c20d64b6cdda9733f24e0c8b7b7d04
unknown <nicolima200@gmail.com>
2022-06-21 21:51:03 -0300

    nuevo usuario

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -55,7 +55,12 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var obj={
+    nombre:nombre,
+    email:email,
+    password:password
+  }
+  return obj;
 }
 
 function tieneEmail (usuario) {
