641073b7c696276d679f4197886ab7dcee717010
unknown <nicolima200@gmail.com>
2022-06-21 21:52:30 -0300

    tiene email

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -67,6 +67,7 @@ function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  return !!usuario.email;
 }
 
 function tienePropiedad (objeto, propiedad) {
