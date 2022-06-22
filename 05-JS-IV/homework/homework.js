ed384f71243d7594e0c8d63eceb2295b38310f01
unknown <nicolima200@gmail.com>
2022-06-21 22:37:03 -0300

    verifica pass

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -83,6 +83,7 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  return usuario.password===password;
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
