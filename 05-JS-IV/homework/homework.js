700cfbaaa026c5a41f059139506a27e678697aed
unknown <nicolima200@gmail.com>
2022-06-21 21:22:31 -0300

    Probando4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,7 +6,7 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-  var gato={
+  var cat={
     nombre: nombre,
     edad: edad
     }  
