ed265a48d5bb276bd5ae95b587575f972ef07fc5
unknown <nicolima200@gmail.com>
2022-06-21 23:22:53 -0300

    A porcentajeDeDescuento le faltaba la referencia al objeto que lo invocaba

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -140,7 +140,7 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
   producto.calcularPrecioDescuento=function(){
-    return producto.precio-producto.precio*porcentajeDeDescuento;
+    return producto.precio-producto.precio*producto.porcentajeDeDescuento;
   };
   return producto;
 }
