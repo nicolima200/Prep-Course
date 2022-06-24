ffae6ca64520e388453017933fcd1dbcd4603ced
unknown <nicolima200@gmail.com>
2022-06-24 00:32:36 -0300

    map

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -42,8 +42,7 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var array1=[]
-  array.forEach(val=>array1.push(cb(val))    )
+  var array1=array.map(cb(val))
 }
 
 function filter(array) {
