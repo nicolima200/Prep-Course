4370b789b9d6a6dbf711bee84664b9f38cc7c48f
unknown <nicolima200@gmail.com>
2022-06-24 01:05:49 -0300

    filter

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -52,7 +52,8 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  var filtro=array.filter(palabra=>palabra.startsWith('a'))
+  var filtro=array.filter(palabra=>palabra.startsWith('a'));
+  return filtro;
 }
 
 // No modificar nada debajo de esta línea
