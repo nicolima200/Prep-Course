511142ee5390a5295c6dc0cea38c09c2edf78d2f
unknown <nicolima200@gmail.com>
2022-06-23 22:48:38 -0300

    Constructor persona 8

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -62,18 +62,18 @@ function agregarStringInvertida() {
 
   class Persona {
     constructor(nombre,apellido,edad,domicilio) {
-      this.nombre=nombre,
-      this.apellido=apellido,
-      this.edad=edad,
-      this.domicilio=domicilio
+      this.nombre=nombre;
+      this.apellido=apellido;
+      this.edad=edad;
+      this.domicilio=domicilio;
       this.detalle=function(){
         return {
           nombre:this.nombre,
           apellido:this.apellido,
           edad:this.edad,
           domicilio:this.domicilio
-        }     
-    }
+        };     
+    };
     
     
     }
