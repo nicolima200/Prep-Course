bbb779e23421e1c662aca0137b8c43d6d25c8a90
unknown <nicolima200@gmail.com>
2022-06-23 23:20:58 -0300

    crearInstanciaPersona

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -68,13 +68,14 @@ function agregarStringInvertida() {
 
   }
   Persona.prototype.detalle=function(){
-    return Persona
+    return this;
   }
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  var persona=new Persona("Juan","Perez","22","Saavedra 123")
 }
   
 function agregarMetodo() {
